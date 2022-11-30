651c29e2249942bb0045a4b4c3cac0cbe49deabc
Michelle Lang Baratto <mi.baratto@gmail.com>
2022-11-30 15:16:10 +0100

    exercico-aula-condicionais1

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// //PRATICA GUIADA 1
+// console.log("PRÁTICA GUIADA 1-----------")
+
+// const boleano1 = false
+// const boleano2 = true
+// const boleano3 = true
+
+// // if(boleano1){
+// //     alert("Bolean é falso")
+// // }else{
+// //     alert("Booleano é falso")
+// // }
+
+// const verfica = false === false 
+// console.log(verfica)
+
+// const verfica2 = false || false
+// console.log(verfica2)
+
+// const verfica3 = false && false
+// console.log(verfica3)
+
+// //PRATICA GUIADA 2
+// console.log("PRÁTICA GUIADA 2-----------")
+
+// const idade = 25
+
+// if(){
+
+// }else if(){
+
+// }esle if()
+
+// }
+
+// //PRATICA GUIADA 4
+console.log("PRÁTICA GUIADA 4-----------")
+
+const media = true
+
+if(media >= 5){
+    console.log("Você foi aprovada/o")
+} else if(media >= 3 && media < 5) {
+    console.log("Você esta em recuperação ")
+} else if(media < 3){
+    console.log("Você foi reprovado/a :(")
+}else {
+    console.log("Você digitou um dado inválido")
+}
+
+const numero = 3 + true
+console.log(numero)
